aae287af2695dbb87718f69ddda5766ed5f47b83
Nguyen Hiep <hiep.nguyenvan@sekisho-vn.com>
2024-06-21 10:46:12 +0900

    tạo thêm các chức năng của message controller, chú ý  các tạo quan hệ với message khi tạo mới, khi xóa, khi tạo url , lấy thông tin từ database
    khi delete room thì có delete thêm những gì ?
    
    cài đặt ngôn ngữ trong cấu hình application.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,11 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
+  has_many :messages, dependent: :destroy
   #   Với mối quan hệ has_many :users, through: :room_users, bạn có thể truy vấn tất cả người dùng trong một phòng một cách đơn giản:
   #   room = Room.find(1)
   # users_in_room = room.users
   validates :name, presence: true
+  
+
 end
